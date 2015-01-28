c6b6a18cf14ba8640e4645abd3f285b3568ab196
besil <s.bernardinello@reply.it>
2015-01-28 14:25:44 +0100

    Starting weight for name and surname

--- src/main/StringMatcherMain.py
+++ src/main/StringMatcherMain.py
@@ -23,4 +23,15 @@ if __name__ == '__main__':
     pprint( sm.presence )
     
     
+#                 p1 = chunk[0]
+#                 p2 = chunk[1]
+#                 diname       = min( di, self.match( self.name(ni), p1 ), self.match(self.name(ni), p2) )
+#                 disurname    = min( di, self.match( self.surname(ni), p1 ), self.match(self.surname(ni), p2) )
+#                 djname       = min( dj, self.match( self.name(mj), p1 ), self.match(self.name(mj), p2) )
+#                 djsurname    = min( dj, self.match( self.surname(mj), p1 ), self.match(self.surname(mj), p2) )
+#                 ditot = diname + disurname + 1
+#                 djtot = djname + djsurname + 1
+#                 di = 0.3*( diname/ditot ) + 0.7 * ( disurname / ditot )
+#                 dj = 0.3*( djname/djtot ) + 0.7 * ( djsurname / djtot )
+    
     

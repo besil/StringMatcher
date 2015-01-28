4b0d3c2e719600c66c44f6527f6c0fc591cc6a31
besil <s.bernardinello@reply.it>
2015-01-28 11:58:12 +0100

    Windower implemented

--- src/main/StringMatcherMain.py
+++ src/main/StringMatcherMain.py
@@ -5,15 +5,24 @@ Created on Jan 27, 2015
 '''
 
 from matcher import StringMatcher
+from pprint import pprint
+
+def windower(l):
+    for i in range(len(l)-1):
+        j = i+1
+        yield ( l[i], l[j] )
 
 if __name__ == '__main__':
     sm = StringMatcher()
     data = "data.txt"
     name_file = 'names.csv'
     
-    text = sm.read( data )
-    print text
+    names = sm.read_names(name_file)
+    pprint(names)
+    document = sm.read( data )
+    print document
     
-    s1 = "ciao"
-    s2 = "mondo"
-    print sm.match( s1, s2 )
+    for chunk in windower(document):
+        sm.match_name(chunk)
+    
+    
--- src/main/matcher.py
+++ src/main/matcher.py
@@ -11,15 +11,26 @@ class StringMatcher(object):
     '''
     A simple wrapper to Levenshtein module
     '''
-
-
     def __init__(self ):
-        pass
+        self.correlation_map = dict()
+    
+    def match_name(self, chunk):
+        new_name = ' '.join(chunk)
+        rev_new_name = ' '.join(chunk[::-1])
     
     def match(self, s1, s2):
         return lev.distance(s1, s2)
     
+    def read_names(self, name_file):
+        for line in open(name_file):
+            split = line.lower().strip().split(",")
+            first = split[0]
+            second = split[1]
+            self.correlation_map[ (first, second) ] = 0
+        return self.correlation_map
+    
     def read(self, data):
+        ''' NON ORA '''
         text = ''.join( [ x.lower() for x in open( data, 'r' ).readlines() ] ).replace("\n", " ")
         return text.translate(string.maketrans("",""), string.punctuation).split(" ")
         
